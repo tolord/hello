dd25d6f3db138cd892c9af1794f2dba0ed11fd7b
liaozhongru <liao.zhongru@foxmail.com>
2016-06-07 23:58:33 +0800

    6.7.23--我添加了一个hello的工程

---
+++ hello/hello/main.cpp
@@ -0,0 +1,19 @@
+//
+//  main.cpp
+//  hello
+//
+//  Created by 廖忠儒 on 16/6/7.
+//  Copyright © 2016年 廖忠儒. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    
+    for (int i=0; i<100; ++i) {
+        printf("hello，world");
+    }
+    return 0;
+}
